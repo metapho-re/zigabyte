8d269a86c6fbb3b4e66871a42bb46a307cb37c86
Pierre-Alain Castella <pierrealain.castella@gmail.com>
2026-05-13 18:06:02 +0200

    fix: Use posix.SIG instead of os.linux.SIG in resize handler.

--- src/terminal.zig
+++ src/terminal.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 const atomic = std.atomic;
 const mem = std.mem;
-const os = std.os;
 const posix = std.posix;
 const testing = std.testing;
 const Sigaction = posix.Sigaction;
@@ -10,7 +9,7 @@ const Winsize = std.posix.winsize;
 
 var resize_event_flag = atomic.Value(bool).init(false);
 
-fn handleResizeEvent(_: os.linux.SIG) callconv(.c) void {
+fn handleResizeEvent(_: posix.SIG) callconv(.c) void {
     resize_event_flag.store(true, .release);
 }
 
